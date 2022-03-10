261404d5e8e17c7dda6a4603cdc7820cdd26716a
Marcela Silverio <marcelapsilverio@gmail.com>
2022-03-10 16:23:06 -0300

    vamos que vamos

--- tic-tac-toe-api/src/controllers/routes.py
+++ tic-tac-toe-api/src/controllers/routes.py
@@ -1,43 +1,73 @@
+from flask import request
 from src.server.instance import server
 from uuid import uuid4
 from random import choice
 
 app, api = server.app, server.api
+games = []
 
-
-@app.route('/', methods=['GET'])
-def home(): # Acho que não vai ter
-    pass
-    #renderizar home com opções ->
-    # Criar nova partida
-    # Entrar em uma partida Existente
 
 @app.route("/game", methods=["POST"])
 def createGame():
     """ Create a new game """
-    first_symbol = choice(["X", "O"])
-    game_id = uuid4()
 
-    return {"id": game_id, "firstPlayer": first_symbol}
+    global games
+    symbol = choice(["X", "O"])
+    new_id = True
 
-@app.route('/game/{id}', methods=['GET']) #talvez post tbm por conta do numero de jogadores
-def enterGame():
-    pass
-    # Renderizar tabuleiro ->
-    # Partida Inexistente
-    # Arquivo com a situação atual da partida
-    # Limitar a apenas dois jogadores
+    while new_id:
+        new_id = False
+        game_id = str(uuid4())
+        for game in games:
+            if game_id in game:
+                new_id = True
 
-@app.route('/game/{id}/leave')
-def leaveGame():
-    pass
-    # Jogo acaba
+    games.append([game_id, symbol, [[0]*3]*3])
+    return {"id": game_id, "firstPlayer": symbol}
 
 
-@app.route("/game/{id}/movement", methods=["POST"])
+@app.route("/game/situation", methods=["GET"])
+def gameSituation(id=None):
+    """ Send the game situation """
+    
+    global games
+    id = request.args.get("id")
+    exist = False
+
+    for game in games:
+        if id in game:
+            return {"turno": game[1], "tabuleiro": game[2]}
+        
+    return {"msg": "Partida não encontrada"}
+
+
+@app.route("/game/movement", methods=["POST"])
 def movement():
-    pass
+    
+    global games
+    id = request.args.get("id")
 
+    for game in games:
+        if id in game:           
+            body = request.get_json()  
+
+            if game[1] != body["player"]:
+                return {"msg": "Não é turno do jogador"}
+            elif game[2][body["tabuleiro"]["y"]-2][body["tabuleiro"]["x"]] == 1:   
+                return {"msg": "Posição indisponível"}
+            else:
+                game[2][body["tabuleiro"]["y"]-2][body["tabuleiro"]["x"]] = 1
+                
+                if  game[1] == "X":
+                    game[1] = "O"
+                else:
+                    game[1] = "X"
+                
+                return {"código": "200"}
+            
+        
+    return {"msg": "Partida não encontrada"}
+    
     # Analisar movimento
     # Turno Errado
     # Chegamos ao Fim -> Velha ou Empate
--- tic-tac-toe-api/src/server/instance.py
+++ tic-tac-toe-api/src/server/instance.py
@@ -3,12 +3,12 @@ from flask_restx import Api
 
 class Server():
     def __init__(self):
-        self.app = Flask('tic-tac-toe')
+        self.app = Flask("tic-tac-toe")
         self.api = Api(self.app,
-            version = '1.0',
-            title = 'tic-tac-toe API',
-            description = 'A simple multiplayer tic-tac-toe game',
-            doc = '/docs'
+            version = "1.0'",
+            title = "tic-tac-toe API",
+            description = "A simple multiplayer tic-tac-toe game",
+            doc = "/docs"
         )
     
     def run(self):
